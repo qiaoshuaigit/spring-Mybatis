bd5ad28ca5780863a62430036c59533a504e7c10
乔帅 <qsxcc@126.com>
2018-03-29 13:45:16 +0800

    设定给定数量得许可证

--- src/main/java/com/qs/web/fruit/thread/TestSemaphore.java
+++ src/main/java/com/qs/web/fruit/thread/TestSemaphore.java
@@ -6,28 +6,33 @@ import java.util.concurrent.Semaphore;
 /**
  * 测试如何限制线程并发数量
  *
+ * 来自java并发编程 核心方法与框架
+ *
+ * 此类不保证线程得安全性
+ *
  * @author shuaion 2018/3/28
  **/
 public class TestSemaphore {
-    //最多允许1个线程同时执行
-    private static Semaphore semaphore = new Semaphore(1);
+
+    //最多允许2个线程同时执行 ture 代表公平竞争模式 先进先出FIFO
+    private static Semaphore semaphore = new Semaphore(10,true);
 
     public static void study(){
         try {
-            semaphore.acquire();
+            semaphore.acquire(5);//从该信号量获取给定数量的许可证，阻止直到所有可用，否则线程为interrupted  10/5=2 每次允许2个线程通过
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("线程名称:"+Thread.currentThread().getName()+",time="+ new Date().getTime());
+        System.out.println("线程名称:"+Thread.currentThread().getName()+",begin time="+ new Date().getTime());
 
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("线程名称:"+Thread.currentThread().getName()+",time="+ new Date().getTime());
+        System.out.println("线程名称:"+Thread.currentThread().getName()+",end time="+ new Date().getTime());
 
-        semaphore.release();
+        semaphore.release(5);//释放信号灯 +1
     }
 
 
@@ -40,17 +45,15 @@ public class TestSemaphore {
     }
 
 
+    
 
     public static void main(String[] args) {
 
-        Thread a = new Student();
-        a.start();
-
-        Thread b = new Student();
-        b.start();
-
-        Thread c = new Student();
-        c.start();
+        Student[] students = new Student[10];
+        for (int i=0;i<students.length;i++){
+            students[i] = new Student();
+            students[i].start();
+        }
 
     }
 
